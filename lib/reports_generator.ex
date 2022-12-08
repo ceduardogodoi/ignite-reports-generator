65f133be7411f057c89d983e5562ef5e08c8e53e
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-12-08 08:38:15 -0300

    Conhecendo o pipe operator

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,8 +1,10 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    case File.read("reports/#{filename}") do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-    end
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file("banana")
   end
+
+  defp handle_file({:ok, file_content}, msg), do: file_content <> msg
+  defp handle_file({:error, _reason}, msg), do: "Error while opening the file." <> msg
 end
